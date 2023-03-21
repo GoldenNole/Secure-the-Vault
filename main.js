9e120e4fabe044875ba399675b29572b75830e5f
Patrick Stokes <Prstokes37@gmail.com>
2023-03-20 19:27:28 -0500

    First Commit!

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log('js is created');
+alert("You have received this message because you have been chosen to open an important vault.");
+let num1 = 40/4
+let num2 = 5*8
+let num3 = 100-61
+alert(`The combination is ${num1} - ${num2} - ${num3}!`)
